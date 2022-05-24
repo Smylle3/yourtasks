969c78971aee7f3a91fa66f411993c22b8b56545
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-05-24 19:22:36 -0300

    Configura rotas básicas ao projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/login/login";
+import Chats from "./components/chats/chats";
+
+// import { AuthProvider } from "../contexts/AuthContext"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ fontFamily: "Avenir" }}>
+      <BrowserRouter>
+        {/* <AuthProvider> */}
+        <Routes>
+          <Route path="/chats" element={<Chats />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
+        {/* </AuthProvider> */}
+      </BrowserRouter>
     </div>
   );
 }
