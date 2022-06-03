1b68b21731f22f2c32a688093311a9f3d17770c4
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-06-03 13:34:23 -0300

    Adiciona novas rotas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TodoList from "./components/todoList/todoList";
 import Login from "./components/login/login";
-import Chats from "./components/chats/chats";
+import Pomodoro from "./components/pomodoro/pomodoro";
+import Calendar from "./components/calendar/calendar";
 
-// import { AuthProvider } from "../contexts/AuthContext"
-
+import { AuthProvider } from "./context/authContext";
+import NavBar from "./components/navBar/navBar";
 
 function App() {
-  return (
-    <div style={{ fontFamily: "Avenir" }}>
-      <BrowserRouter>
-        {/* <AuthProvider> */}
-        <Routes>
-          <Route path="/chats" element={<Chats />} />
-          <Route path="/" element={<Login />} />
-        </Routes>
-        {/* </AuthProvider> */}
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div>
+            <BrowserRouter>
+                <AuthProvider>
+                    <NavBar />
+                    <Routes>
+                        <Route path="/" element={<TodoList />} />
+                        <Route path="/calendar" element={<Calendar />} />
+                        <Route path="/pomodoro" element={<Pomodoro />} />
+                        <Route path="/login" element={<Login />} />
+                    </Routes>
+                </AuthProvider>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
