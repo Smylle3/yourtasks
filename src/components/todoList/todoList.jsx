f239f57d8e6878f41178c980e63b690147e6b69e
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-06-03 21:39:35 -0300

    Adiciona loading antes de carregar informações

--- src/components/todoList/todoList.jsx
+++ src/components/todoList/todoList.jsx
@@ -3,8 +3,11 @@ import React, { useState } from "react";
 import MyModal from "./myModal";
 import EmptyImage from "../../assets/EmptyImage.jpg";
 import "./styles.css";
+import { useAuth } from "../../context/authContext";
+import { Spin } from "antd";
 
 export default function TodoList() {
+    const { user } = useAuth();
     const [isModalVisible, setIsModalVisible] = useState(false);
     const [typeModal, setTypeModal] = useState(false);
     const [deleteId, setDeleteId] = useState(false);
@@ -30,50 +33,64 @@ export default function TodoList() {
         setIsModalVisible(false);
     };
 
-    return (
-        <div className="todo-page">
-            <form className="todo-form">
-                <div className="todo-input" onClick={() => showModal("create")}>
-                    <p>Insira aqui uma nova tarefa...</p>
-                    <EnterOutlined style={{ cursor: "pointer" }} />
-                </div>
-            </form>
-            {allTask.length > 0 ? (
-                <nav className="todo-tasks">
-                    {allTask.map((task, id) => (
-                        <section className="todo-task" key={id}>
-                            <div
-                                onClick={() => showModal("info", id)}
-                                className="todo-content-info"
-                            >
-                                <h3>{task.title}</h3>
-                                <p>Até: {task.date}</p>
-                            </div>
-                            <button>
-                                <CheckOutlined />
-                            </button>
-                            <button onClick={() => showModal("delete", id)}>
-                                <DeleteFilled />
-                            </button>
-                        </section>
-                    ))}
-                </nav>
-            ) : (
-                <img className="emprty-image" alt="backImg" src={EmptyImage} />
-            )}
-            <MyModal
-                isModalVisible={isModalVisible}
-                handleOk={handleOk}
-                handleCancel={handleCancel}
-                setIsModalVisible={setIsModalVisible}
-                task={task}
-                setTask={setTask}
-                allTask={allTask}
-                setAllTask={setAllTask}
-                typeModal={typeModal}
-                deleteId={deleteId}
-            />
-        </div>
-    );
+    if (user) {
+        return (
+            <div className="todo-page">
+                <form className="todo-form">
+                    <div
+                        className="todo-input"
+                        onClick={() => showModal("create")}
+                    >
+                        <p>Insira aqui uma nova tarefa...</p>
+                        <EnterOutlined style={{ cursor: "pointer" }} />
+                    </div>
+                </form>
+                {allTask.length > 0 ? (
+                    <nav className="todo-tasks">
+                        {allTask.map((task, id) => (
+                            <section className="todo-task" key={id}>
+                                <div
+                                    onClick={() => showModal("info", id)}
+                                    className="todo-content-info"
+                                >
+                                    <h3>{task.title}</h3>
+                                    <p>Até: {task.date}</p>
+                                </div>
+                                <button>
+                                    <CheckOutlined />
+                                </button>
+                                <button onClick={() => showModal("delete", id)}>
+                                    <DeleteFilled />
+                                </button>
+                            </section>
+                        ))}
+                    </nav>
+                ) : (
+                    <img
+                        className="emprty-image"
+                        alt="backImg"
+                        src={EmptyImage}
+                    />
+                )}
+                <MyModal
+                    isModalVisible={isModalVisible}
+                    handleOk={handleOk}
+                    handleCancel={handleCancel}
+                    setIsModalVisible={setIsModalVisible}
+                    task={task}
+                    setTask={setTask}
+                    allTask={allTask}
+                    setAllTask={setAllTask}
+                    typeModal={typeModal}
+                    deleteId={deleteId}
+                />
+            </div>
+        );
+    } else {
+        return (
+            <div className="spin">
+                <Spin />
+            </div>
+        );
+    }
 }
-
