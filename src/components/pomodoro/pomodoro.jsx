26541518624ed4426bcac4c78379a2c2ec26c0e7
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-06-17 20:47:41 -0300

    Inicia o pomodoro

--- src/components/pomodoro/pomodoro.jsx
+++ src/components/pomodoro/pomodoro.jsx
@@ -1,9 +1,18 @@
+import { Progress } from "antd";
 import React from "react";
+import "./styles.css";
 
 export default function Pomodoro() {
-  return (
-    <div className="home-page">
-      Pomodoro
-    </div>
-  );
+    return (
+        <div className="pomo-page">
+            <p>01:05:25</p>
+            <Progress
+                strokeColor={{
+                    "0%": "#108ee9",
+                    "100%": "#87d068",
+                }}
+            />
+            <button>Start</button>
+        </div>
+    );
 }
