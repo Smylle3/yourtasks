e268cb1b7c8d043c93b5a444bda36079cdf67d67
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-06-17 22:04:04 -0300

    Adiciona e configura botão de tabs

--- src/components/todoList/todoList.jsx
+++ src/components/todoList/todoList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import { useAuth } from "../../context/authContext";
 import { Spin } from "antd";
@@ -6,22 +6,37 @@ import "moment/locale/pt-br";
 import MenuPlus from "../menuPlus/menuPlus";
 import Todo from "./todo/todo";
 import Done from "./done/done";
-import useMobile from "../../functions/useMobile";
+import { BorderOutlined, CheckSquareOutlined } from "@ant-design/icons";
 
 export default function TodoList() {
     const { user } = useAuth();
-    const isMobile = useMobile();
+    const [doneTab, setDoneTab] = useState("to do");
 
     if (user) {
         return (
             <div className="todo-page">
                 <form className="todo-form">
-                    <MenuPlus />
-                </form>
-                    <div className="todo-lists">
-                        <Todo />
-                        <Done />
+                    <div
+                        className={`default-done-tab ${
+                            doneTab === "to do" ? `done-tab` : null
+                        }`}
+                        onClick={() => setDoneTab("to do")}
+                    >
+                        <BorderOutlined /> A fazer
                     </div>
+                    <MenuPlus />
+                    <div
+                        className={`default-done-tab ${
+                            doneTab === "done" ? `done-tab` : null
+                        }`}
+                        onClick={() => setDoneTab("done")}
+                    >
+                        <CheckSquareOutlined /> Feitos
+                    </div>
+                </form>
+                <div className="todo-lists">
+                    {doneTab === "to do" ? <Todo /> : <Done />}
+                </div>
             </div>
         );
     } else {
