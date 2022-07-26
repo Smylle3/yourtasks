e20ebf225c2300829c5e746d93dbef2ff1ac3e2b
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-07-26 17:58:12 -0300

    Torna apenas uma opção de task a ser criada

--- src/components/menuPlus/menuPlus.jsx
+++ src/components/menuPlus/menuPlus.jsx
@@ -1,53 +1,26 @@
 import { PlusOutlined } from "@ant-design/icons";
-import { Popover } from "antd";
 import React, { useState } from "react";
 import CreateTask from "../todoList/modais/createTask";
 import "./styles.css";
 
 export default function MenuPlus() {
     const [type, setType] = useState("");
-    const [visible, setVisible] = useState(false);
     const [isModalVisible, setModalVisible] = useState(false);
 
     const modalFunction = (type) => {
-        setVisible(false);
         setType(type);
         setModalVisible(true);
-    };
-    
-    const content = (
-        <div className="task-menu-plus">
-            <button
-                className="logout-button"
-                onClick={() => modalFunction("detail")}
-            >
-                Tarefa com detalhes
-            </button>
-            <button
-                className="logout-button"
-                onClick={() => modalFunction("simple")}
-            >
-                Lista simples
-            </button>
-        </div>
-    );
-
-    const handleVisibleChange = (newVisible) => {
-        setVisible(newVisible);
     };
 
     return (
         <>
-            <Popover
-                onVisibleChange={handleVisibleChange}
-                visible={visible}
-                content={content}
-                trigger="click"
+            <div
+                onClick={() => modalFunction("detail")}
+                className="todo-input-plus"
             >
-                <div className="todo-input-plus">
-                    <PlusOutlined />
-                </div>
-            </Popover>
+                <PlusOutlined />
+            </div>
+
             <CreateTask
                 isModalVisible={isModalVisible}
                 setModalVisible={setModalVisible}
