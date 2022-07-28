b285e94dda988adead5fc1716144a67f7bff2776
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-07-27 21:47:18 -0300

    Remove funções e otimiza app

--- src/components/pomodoro/pomodoro.jsx
+++ src/components/pomodoro/pomodoro.jsx
@@ -6,67 +6,28 @@ import {
 import { Dropdown, InputNumber, Menu, Popover, Progress } from "antd";
 import React, { useState } from "react";
 import { useEffect } from "react";
+import { useAuth } from "../../context/authContext";
 import "./styles.css";
 
 export default function Pomodoro() {
-    const [timer, setTimer] = useState(false);
-    const [isStart, setIsStart] = useState(false);
-    const [min, setMin] = useState(0);
-    const [sec, setSec] = useState(0);
-    const [prog, setProg] = useState(0);
-    const [finalTime, setFinalTime] = useState(25);
-    const [finalSleep, setFinalSleep] = useState(5);
-    const [timerFunction, setTimerFunction] = useState("work");
-
-    let seconds = 0;
-    let minutes = 0;
-    let progress = 0;
-
-    useEffect(() => {
-        if (min === finalTime && timerFunction === "work") {
-            stopTimer();
-            setTimerFunction("sleep");
-            startTimer();
-        }
-        if (min === finalSleep && timerFunction === "sleep") {
-            stopTimer();
-            setTimerFunction("work");
-            startTimer();
-        }
-    }, [min]);
-
-    function startTimer() {
-        setIsStart(true);
-        setTimer(setInterval(() => showTime(), 1000));
-    }
-    function stopTimer() {
-        clearInterval(timer);
-        setMin(0);
-        setSec(0);
-        setProg(0);
-        setIsStart(false);
-        setTimerFunction("work");
-    }
-
-    function showTime() {
-        seconds++;
-        progress++;
-        setProg(progress);
-        setSec(seconds);
-        if (seconds > 59) {
-            seconds = 0;
-            minutes++;
-            setSec(seconds);
-            setMin(minutes);
-        }
-    }
+    const {
+        finalTime,
+        setFinalSleep,
+        setFinalTime,
+        finalSleep,
+        timerFunction,
+        timerObj,
+        startTimer,
+        stopTimer,
+        timer,
+    } = useAuth();
 
     const menu = (
         <div className="set-timers">
             <p>
                 Concentrar (min):
                 <InputNumber
-                    disabled={isStart}
+                    disabled={timer}
                     min={1}
                     max={60}
                     bordered={false}
@@ -80,7 +41,7 @@ export default function Pomodoro() {
             <p>
                 Descansar (min):
                 <InputNumber
-                    disabled={isStart}
+                    disabled={timer}
                     min={1}
                     max={60}
                     bordered={false}
@@ -105,7 +66,8 @@ export default function Pomodoro() {
                     : `Tempo para descansar: ${finalSleep} min`}
             </text>
             <h1>
-                {min < 10 ? `0${min}` : min}:{sec < 10 ? `0${sec}` : sec}
+                {timerObj.min < 10 ? `0${timerObj.min}` : timerObj.min}:
+                {timerObj.sec < 10 ? `0${timerObj.sec}` : timerObj.sec}
             </h1>
             <div className="set-config">
                 <Popover content={menu} trigger={["click"]} placement="bottom">
@@ -115,7 +77,7 @@ export default function Pomodoro() {
             <div className="progress-bar">
                 <Progress
                     percent={Math.trunc(
-                        (prog * 100) /
+                        (timerObj.prog * 100) /
                             (timerFunction === "work"
                                 ? finalTime * 60
                                 : finalSleep * 60)
@@ -135,7 +97,7 @@ export default function Pomodoro() {
                 />
             </div>
             <section className="button-section">
-                {!isStart ? (
+                {!timer ? (
                     <button
                         className="confirm-button-modal my-button"
                         onClick={() => startTimer()}
