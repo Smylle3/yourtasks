db85bf832dc1711388f3d8d56d0bd839c3d07d7c
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-08-06 21:32:39 -0300

    Faz chamada ao firestore

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
@@ -11,8 +12,9 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
 const gitProvider = new GithubAuthProvider();
 
-export { app, auth, googleProvider, gitProvider };
+export { app, auth, googleProvider, gitProvider, db };
