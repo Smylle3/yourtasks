71eebc833d44273bc6fc6c7f5cb8b2e58dbc29f0
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-08-08 20:17:41 -0300

    Adiciona funções content pomodoro

--- src/components/myPopoversContent/myPopoversContent.jsx
+++ src/components/myPopoversContent/myPopoversContent.jsx
@@ -1,4 +1,9 @@
-import { LogoutOutlined, SettingOutlined } from "@ant-design/icons";
+import {
+    LogoutOutlined,
+    QuestionCircleOutlined,
+    SettingOutlined,
+} from "@ant-design/icons";
+import { Divider, InputNumber, Tooltip } from "antd";
 import { signOut } from "firebase/auth";
 import React from "react";
 import { auth } from "../../config/firebase";
@@ -7,6 +12,15 @@ function handleLogout() {
     signOut(auth);
 }
 
+const clicleText = (finalTime, timerObj, finalSleep) => (
+    <span className="cicle-text">
+        Um ciclo é o tempo de uma Concentração. Atualmente um ciclo equivale à{" "}
+        {finalTime} minutos e, o total de {timerObj.cicles} ciclo(s) equivale à{" "}
+        {finalTime * timerObj.cicles + (timerObj.cicles - 1) * finalSleep}{" "}
+        minutos.
+    </span>
+);
+
 const userOptionsContent = (
     TypeStorage,
     setVisible,
@@ -14,7 +28,7 @@ const userOptionsContent = (
     turnLocalToCloud,
     isMobile
 ) => (
-    <div className="settings-user-menu">
+    <section className="settings-user-menu">
         <button
             className="logout-button"
             onClick={() => {
@@ -33,7 +47,105 @@ const userOptionsContent = (
         >
             <LogoutOutlined className="icon-logout" /> LogOut
         </button>
+    </section>
+);
+
+const pomodoroTaskListContent = (
+    visiblePopover,
+    setVisiblePopover,
+    setTaskSelected,
+    allTask
+) => (
+    <section className="task-list">
+        <h3
+            onClick={() => {
+                setVisiblePopover(!visiblePopover);
+                setTaskSelected({ id: -2, task: "Apenas concentração" });
+            }}
+        >
+            Apenas concentração
+        </h3>
+        <Divider>Suas tasks</Divider>
+        {allTask.map((task, id) => (
+            <section
+                className=""
+                key={id}
+                onClick={() => {
+                    setVisiblePopover(!visiblePopover);
+                    setTaskSelected({ id: id, task: task.title });
+                }}
+            >
+                <h3>
+                    {id + 1} - {task.title}
+                </h3>
+            </section>
+        ))}
+    </section>
+);
+
+const pomodoroSetTimerContent = (
+    timer,
+    finalTime,
+    setFinalTime,
+    finalSleep,
+    setFinalSleep,
+    timerObj,
+    setTimerObj
+) => (
+    <div className="set-timers">
+        <span>
+            Concentrar (min):
+            <InputNumber
+                disabled={timer}
+                min={1}
+                max={60}
+                bordered={false}
+                defaultValue={finalTime}
+                onChange={(e) => setFinalTime(e)}
+                autoFocus
+                controls={false}
+                size="small"
+            />
+        </span>
+        <span>
+            Descansar (min):
+            <InputNumber
+                disabled={timer}
+                min={1}
+                max={60}
+                bordered={false}
+                defaultValue={finalSleep}
+                onChange={(e) => setFinalSleep(e)}
+                controls={false}
+                size="small"
+            />
+        </span>
+        <span>
+            Número de ciclos{" "}
+            <Tooltip
+                placement="top"
+                title={clicleText(finalTime, timerObj, finalSleep)}
+            >
+                <QuestionCircleOutlined />
+            </Tooltip>
+            :
+            <InputNumber
+                disabled={timer}
+                min={1}
+                max={60}
+                bordered={false}
+                defaultValue={timerObj.cicles}
+                onChange={(e) =>
+                    setTimerObj((prevState) => ({
+                        ...prevState,
+                        cicles: e,
+                    }))
+                }
+                controls={false}
+                size="small"
+            />
+        </span>
     </div>
 );
 
-export { userOptionsContent };
+export { userOptionsContent, pomodoroTaskListContent, pomodoroSetTimerContent };
