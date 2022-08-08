284d52dd51377e7adb73dd95d5e586f08dfbc8f8
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-08-08 20:18:03 -0300

    Refatora pomodoro

--- src/components/pomodoro/pomodoro.jsx
+++ src/components/pomodoro/pomodoro.jsx
@@ -4,134 +4,50 @@ import {
     CaretUpOutlined,
     CloseOutlined,
     EditOutlined,
-    QuestionCircleOutlined,
 } from "@ant-design/icons";
-import { Divider, InputNumber, Popover, Progress, Tooltip } from "antd";
+import { Popover, Progress } from "antd";
 import React, { useState } from "react";
 import { useAuth } from "../../context/authContext";
 import { usePomodoro } from "../../context/pomodoroContext";
+import {
+    pomodoroSetTimerContent,
+    pomodoroTaskListContent,
+} from "../myPopoversContent/myPopoversContent";
 import "./styles.css";
 
 export default function Pomodoro() {
     const { allTask } = useAuth();
     const {
         finalTime,
-        setFinalSleep,
-        setFinalTime,
         finalSleep,
         timerFunction,
         timerObj,
-        setTimerObj,
         startTimer,
         stopTimer,
         timer,
         taskSelected,
         setTaskSelected,
+        setFinalTime,
+        setFinalSleep,
+        setTimerObj,
     } = usePomodoro();
 
     const [visiblePopover, setVisiblePopover] = useState(false);
 
-    const clicleText = (
-        <span className="cicle-text">
-            Um ciclo é o tempo de uma Concentração. Atualmente um ciclo equivale
-            à {finalTime} minutos e, o total de {timerObj.cicles} ciclo(s)
-            equivale à{" "}
-            {finalTime * timerObj.cicles + (timerObj.cicles - 1) * finalSleep}{" "}
-            minutos.
-        </span>
-    );
-
-    const taskList = (
-        <div className="task-list">
-            <h3
-                onClick={() => {
-                    setVisiblePopover(!visiblePopover);
-                    setTaskSelected({ id: -2, task: "Apenas concentração" });
-                }}
-            >
-                Apenas concentração
-            </h3>
-            <Divider>Suas tasks</Divider>
-            {allTask.map((task, id) => (
-                <section
-                    className=""
-                    key={id}
-                    onClick={() => {
-                        setVisiblePopover(!visiblePopover);
-                        setTaskSelected({ id: id, task: task.title });
-                    }}
-                >
-                    <h3>
-                        {id + 1} - {task.title}
-                    </h3>
-                </section>
-            ))}
-        </div>
-    );
-
-    const menu = (
-        <div className="set-timers">
-            <span>
-                Concentrar (min):
-                <InputNumber
-                    disabled={timer}
-                    min={1}
-                    max={60}
-                    bordered={false}
-                    defaultValue={finalTime}
-                    onChange={(e) => setFinalTime(e)}
-                    autoFocus
-                    controls={false}
-                    size="small"
-                />
-            </span>
-            <span>
-                Descansar (min):
-                <InputNumber
-                    disabled={timer}
-                    min={1}
-                    max={60}
-                    bordered={false}
-                    defaultValue={finalSleep}
-                    onChange={(e) => setFinalSleep(e)}
-                    controls={false}
-                    size="small"
-                />
-            </span>
-            <span>
-                Número de ciclos{" "}
-                <Tooltip placement="top" title={clicleText}>
-                    <QuestionCircleOutlined />
-                </Tooltip>
-                :
-                <InputNumber
-                    disabled={timer}
-                    min={1}
-                    max={60}
-                    bordered={false}
-                    defaultValue={timerObj.cicles}
-                    onChange={(e) =>
-                        setTimerObj((prevState) => ({
-                            ...prevState,
-                            cicles: e,
-                        }))
-                    }
-                    controls={false}
-                    size="small"
-                />
-            </span>
-        </div>
-    );
-
     return (
         <div className="pomo-page">
-            <div
+            <section
                 className={`type-time ${
                     timerFunction === "work" ? "work" : "sleep"
                 }`}
             >
                 <Popover
-                    content={taskList}
+                    content={pomodoroTaskListContent(
+                        visiblePopover,
+                        setVisiblePopover,
+                        setTaskSelected,
+                        allTask
+                    )}
                     trigger={["click"]}
                     placement="bottom"
                     className="popover-style"
@@ -154,13 +70,25 @@ export default function Pomodoro() {
                         />
                     )}
                 </Popover>
-            </div>
+            </section>
             <h1>
                 {timerObj.min < 10 ? `0${timerObj.min}` : timerObj.min}:
                 {timerObj.sec < 10 ? `0${timerObj.sec}` : timerObj.sec}
             </h1>
-            <div className="set-config">
-                <Popover content={menu} trigger={["click"]} placement="bottom">
+            <section className="set-config">
+                <Popover
+                    content={pomodoroSetTimerContent(
+                        timer,
+                        finalTime,
+                        setFinalTime,
+                        finalSleep,
+                        setFinalSleep,
+                        timerObj,
+                        setTimerObj
+                    )}
+                    trigger={["click"]}
+                    placement="bottom"
+                >
                     {timerFunction === "work" ? (
                         <span>
                             Concentração: {finalTime} min <EditOutlined />
@@ -171,8 +99,8 @@ export default function Pomodoro() {
                         </span>
                     )}
                 </Popover>
-            </div>
-            <div className="progress-bar">
+            </section>
+            <section className="progress-bar">
                 <Progress
                     percent={Math.trunc(
                         (timerObj.prog * 100) /
@@ -193,7 +121,7 @@ export default function Pomodoro() {
                     }
                     strokeWidth={50}
                 />
-            </div>
+            </section>
             <section className="button-section">
                 {!timer ? (
                     <button
