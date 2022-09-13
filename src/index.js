0fa58d8fbc363119333bfd25a48851317c1541e4
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-09-13 14:49:25 -0300

    Importa estilização global

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
 import 'antd/dist/antd.css'
+import App from 'App';
+import { GlobalStyle } from 'globalStyles';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <GlobalStyle />
   </React.StrictMode>,
   document.getElementById('root')
 );
