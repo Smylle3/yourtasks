2d0d8935038fc0e7476bb17df3082f9007f0bd81
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-09-26 15:24:09 -0300

    Altera importações

--- src/components/todoList/done/done.jsx
+++ src/components/todoList/done/done.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import moment from "moment";
 import { Empty } from "antd";
 import { useAuth } from "context/authContext";
-import InfosTask from "../modais/infosTask";
 import { DeleteFilled } from "@ant-design/icons";
 import {
     CheckButton,
@@ -12,6 +11,7 @@ import {
     TaskList,
     TitleTask,
 } from "../stylesTodo";
+import InfosTask from "../modais/infosTask";
 
 export default function Done() {
     const { allTaskDone, setAllTaskDone } = useAuth();
