87e71dfeefae083185e2dba176733859078cf849
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-10-04 08:47:40 -0300

    Corrige botao de adicionar nova tarefa

--- src/components/menuPlus/stylePlus.js
+++ src/components/menuPlus/stylePlus.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 export const PlusButton = styled.button`
     cursor: pointer;
-    width: 3em;
-    height: 3em;
+    min-width: 3em;
+    min-height: 3em;
     border-radius: 50%;
     border: 0 none;
     box-shadow: 3px 3px 15px -5px black;
