9ae9bda89f4d1c9ca7f87fea4b7e83795b1ad922
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-10-06 00:25:34 -0300

    Reduz arquivo e chama novo modelo

--- src/components/todoList/todoList.jsx
+++ src/components/todoList/todoList.jsx
@@ -5,60 +5,15 @@ import { Spin } from "antd";
 import MenuPlus from "components/menuPlus/menuPlus";
 import Todo from "./todo/todo";
 import Done from "./done/done";
-import Carousel from "nuka-carousel/lib/carousel";
 import { BorderOutlined, CheckSquareOutlined } from "@ant-design/icons";
 import { SelectTabButton, SpinDiv, TodoHeader, TodoPage } from "./stylesTodo";
 import "moment/locale/pt-br";
+import MyCarousel from "components/myCarousel/myCarousel";
 
 export default function TodoList() {
     const { user } = useAuth();
     const isMobile = useMobile();
     const [doneTab, setDoneTab] = useState(0);
-
-    const PrevButton = (
-        <>
-            <BorderOutlined />
-            <h3 style={{ margin: "0" }}>A fazer</h3>
-        </>
-    );
-    const NextButton = (
-        <>
-            <CheckSquareOutlined />
-            <h3 style={{ margin: "0" }}>Feitos</h3>
-        </>
-    );
-
-    const Customization = {
-        carousel: {
-            display: "flex",
-            flexDirection: "column-reverse",
-            minHeight: "70vh",
-        },
-        next: {
-            display: "flex",
-            alignItems: "center",
-            gap: ".5em",
-            backgroundColor: "#fff",
-            color: "#000",
-            borderRadius: "5px",
-            border: "1px solid #000",
-            position: "fixed",
-            bottom: "7em",
-            right: "1em",
-        },
-        prev: {
-            display: "flex",
-            alignItems: "center",
-            gap: ".5em",
-            backgroundColor: "#fff",
-            color: "#000",
-            borderRadius: "5px",
-            border: "1px solid #000",
-            position: "fixed",
-            bottom: "7em",
-            left: "1em",
-        },
-    };
 
     if (user) {
         return (
@@ -66,23 +21,10 @@ export default function TodoList() {
                 {isMobile ? (
                     <>
                         <MenuPlus />
-                        <Carousel
-                            adaptiveHeight={false}
-                            style={Customization.carousel}
-                            afterSlide={(e) => setDoneTab(e)}
-                            defaultControlsConfig={{
-                                nextButtonText: NextButton,
-                                nextButtonStyle: Customization.next,
-                                prevButtonStyle: Customization.prev,
-                                prevButtonText: PrevButton,
-                                pagingDotsStyle: {
-                                    fill: "transparent",
-                                },
-                            }}
-                        >
+                        <MyCarousel setDoneTab={setDoneTab}>
                             <Todo />
                             <Done />
-                        </Carousel>
+                        </MyCarousel>
                     </>
                 ) : (
                     <>
