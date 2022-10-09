395bec24ecdbcb441f0c48a1c8fdae3627b728dc
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-10-09 01:25:56 -0300

    Remove componente repetitivo

--- src/components/todoList/modais/createTask.jsx
+++ src/components/todoList/modais/createTask.jsx
@@ -1,196 +1,33 @@
-import { CheckOutlined, DeleteFilled } from "@ant-design/icons";
-import { DatePicker, message, Modal } from "antd";
-import PriorityDropdown from "components/dropdowns/priorityDropdown";
-import { useAuth } from "context/authContext";
 import React, { useState } from "react";
-import {
-    ButtonFooterGroup,
-    ButtonGroup,
-    ButtonModal,
-    ChecklistContent,
-    ChecklistInput,
-    HeaderModal,
-    InfoModal,
-    InputGroup,
-    ModalInput,
-    SimpleButton,
-} from "./stylesModal";
-import TextEditor from "components/textEditor/textEditor";
+import { Modal } from "antd";
+import { useAuth } from "context/authContext";
+import TodoForm from "../todoComponents/todoForm";
 
 export default function CreateTask({ isModalVisible, setModalVisible }) {
-    const { task, setTask, setAllTask } = useAuth();
-    const [hasDate, setHasDate] = useState(false);
-    const [hasCheckList, setHasCheckList] = useState(false);
-    const [change, setChange] = useState(false);
-    const [simpleDescription, setSimpleDescription] = useState({
-        text: "",
-        checked: false,
+    const { task, setTask } = useAuth();
+    const [hasProps, setHasProps] = useState({
+        date: false,
+        checkList: false,
     });
-
-    const handleCancel = () => {
-        setTask({
-            title: "",
-            description: "",
-            checkList: [],
-            date: "",
-        });
-        setSimpleDescription({
-            text: "",
-            checked: false,
-        });
-        setModalVisible(false);
-        setHasDate(false);
-        setHasCheckList(false);
-    };
-
-    function handleDescription(key) {
-        if (
-            simpleDescription.text.length > 0 &&
-            (key === "Enter" || key === "NumpadEnter")
-        ) {
-            task.checkList.push(simpleDescription);
-            setSimpleDescription({ text: "", checked: false });
-        }
-    }
-
-    function handleSend(event) {
-        event.preventDefault();
-        if (task.title.length === 0) {
-            message.error("Preencha os campos corretamente!");
-            return;
-        }
-        setAllTask((arr) => [...arr, task]);
-        setTask({
-            title: "",
-            description: "",
-            checkList: [],
-            date: "",
-            endDate: "",
-            priority: 1,
-        });
-        setModalVisible(false);
-    }
-
-    function editItem(value, index) {
-        task.checkList[index].text = value;
-        setChange(!change);
-    }
 
     return (
         <Modal
             visible={isModalVisible}
-            onCancel={handleCancel}
+            onCancel={() => setModalVisible(false)}
             footer={null}
             closable={false}
             style={{ cursor: "default" }}
         >
-            <InfoModal>
-                <HeaderModal>
-                    <ModalInput
-                        autoFocus
-                        placeholder="Título..."
-                        onChange={(e) =>
-                            setTask((prevState) => ({
-                                ...prevState,
-                                title: e.target.value,
-                            }))
-                        }
-                        value={task.title}
-                        type="2em"
-                        border={"#8080805f"}
-                    />
-                    <PriorityDropdown localTask={task} setLocalTask={setTask} />
-                </HeaderModal>
-                <TextEditor task={task} setTask={setTask} isEdit={true}/>
-                <ButtonGroup>
-                    <SimpleButton
-                        onClick={() => {
-                            setTask((prevState) => ({
-                                ...prevState,
-                                checkList: [],
-                            }));
-                            setSimpleDescription({
-                                text: "",
-                                checked: false,
-                            });
-                            setHasCheckList(!hasCheckList);
-                        }}
-                    >
-                        {hasCheckList
-                            ? `Excluir o checklist`
-                            : `Clique para criar um checklist`}
-                    </SimpleButton>
-                    {hasCheckList && (
-                        <ChecklistInput onSubmit={(e) => e.preventDefault()}>
-                            <ModalInput
-                                onChange={(e) => {
-                                    setSimpleDescription({
-                                        text: e.target.value,
-                                        checked: false,
-                                    });
-                                }}
-                                onKeyDown={(e) => handleDescription(e.code)}
-                                placeholder="Ex.: Item 1"
-                                value={simpleDescription.text}
-                                border="#8080805f"
-                            />
-                            <CheckOutlined
-                                onClick={() => handleDescription("Enter")}
-                                className="check-icon"
-                            />
-                        </ChecklistInput>
-                    )}
-                    {task.checkList.length > 0 && (
-                        <InputGroup>
-                            {task.checkList.map((content, id) => (
-                                <ChecklistContent key={id}>
-                                    <>{id + 1}</>
-                                    <ModalInput
-                                        onChange={(e) =>
-                                            editItem(e.target.value, id)
-                                        }
-                                        placeholder={task.checkList[id].text}
-                                        value={task.checkList[id].text}
-                                    />
-                                    <DeleteFilled
-                                        onClick={() => {
-                                            task.checkList.splice(id, 1);
-                                            setChange(!change);
-                                        }}
-                                    />
-                                </ChecklistContent>
-                            ))}
-                        </InputGroup>
-                    )}
-
-                    <SimpleButton onClick={() => setHasDate(!hasDate)}>
-                        {hasDate
-                            ? `Não definir data para conclusão`
-                            : `Clique para definir uma data para a conclusão`}
-                    </SimpleButton>
-                    {hasDate && (
-                        <DatePicker
-                            showTime={{ format: "HH:mm" }}
-                            className="date-pickeC"
-                            format="YYYY-MM-DD HH:mm"
-                            onChange={(ev, e) =>
-                                setTask((prevState) => ({
-                                    ...prevState,
-                                    date: e,
-                                }))
-                            }
-                        />
-                    )}
-                </ButtonGroup>
-                <ButtonFooterGroup>
-                    <ButtonModal type="cancel" onClick={(e) => handleCancel()}>
-                        FECHAR
-                    </ButtonModal>
-                    <ButtonModal type="confirm" onClick={(e) => handleSend(e)}>
-                        ADICIONAR
-                    </ButtonModal>
-                </ButtonFooterGroup>
-            </InfoModal>
+            <TodoForm
+                isEdit={true}
+                status={"create"}
+                localTask={task}
+                setLocalTask={setTask}
+                isModal={true}
+                setModalVisible={setModalVisible}
+                hasProps={hasProps}
+                setHasProps={setHasProps}
+            />
         </Modal>
     );
 }
--- src/components/todoList/modais/modalTask.jsx
+++ src/components/todoList/modais/modalTask.jsx
@@ -1,34 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useAuth } from "context/authContext";
-import { DatePicker, Modal } from "antd";
-import moment from "moment";
-import { CheckOutlined, DeleteFilled } from "@ant-design/icons";
-import {
-    ChecklistInput,
-    ModalInput,
-    InputGroup,
-    ChecklistContent,
-    IconsModal,
-    Checkbox,
-    LabelModal,
-    ShowDate,
-    InfoModal,
-    ButtonModal,
-    HeaderModal,
-} from "./stylesModal";
-import {
-    editItem,
-    handleChangeCheck,
-    handleDescription,
-    handleEditConfirm,
-} from "functions/tasksEdit";
-import PriorityDropdown from "components/dropdowns/priorityDropdown";
-import { priorityColor, priorityText } from "functions/setPriority";
-import {
-    ContainerPriority,
-    DotPriority,
-} from "components/dropdowns/stylesDropdown";
-import TextEditor from "components/textEditor/textEditor";
+import { Modal } from "antd";
+import TodoForm from "../todoComponents/todoForm";
 
 export default function ModalTask({
     isModalVisible,
@@ -37,14 +10,9 @@ export default function ModalTask({
     setId,
     status,
 }) {
-    const { allTask, allTaskDone, updateDBTasks, change, setChange } =
-        useAuth();
+    const { allTask, allTaskDone } = useAuth();
     const [localTask, setLocalTask] = useState();
     const [isEdit, setIsEdit] = useState(false);
-    const [simpleDescription, setSimpleDescription] = useState({
-        text: "",
-        checked: false,
-    });
 
     useEffect(() => {
         status === "done"
@@ -52,268 +20,27 @@ export default function ModalTask({
             : setLocalTask(allTask[id]);
     }, [id, allTask, allTaskDone, status]);
 
-    const handleCancel = () => {
-        setIsEdit(false);
-        setId(null);
-        setModalVisible(false);
-        setLocalTask();
-    };
-
-    const detailDescription = () => (
-        <TextEditor task={localTask} setTask={setLocalTask} isEdit={isEdit} />
-    );
-
-    const listDescription = (isFinish) => (
-        <>
-            {isEdit && (
-                <ChecklistInput onSubmit={(e) => e.preventDefault()}>
-                    <ModalInput
-                        onChange={(e) => {
-                            setSimpleDescription({
-                                text: e.target.value,
-                                checked: false,
-                            });
-                        }}
-                        onKeyDown={(e) =>
-                            handleDescription(
-                                e.code,
-                                simpleDescription,
-                                setSimpleDescription,
-                                localTask
-                            )
-                        }
-                        placeholder="Add novo item"
-                        value={simpleDescription.text}
-                        type="18px"
-                        border="#8080805f"
-                        autoFocus
-                    />
-                    <CheckOutlined
-                        onClick={() =>
-                            handleDescription(
-                                "Enter",
-                                simpleDescription,
-                                setSimpleDescription,
-                                localTask
-                            )
-                        }
-                        className="check-icon"
-                    />
-                </ChecklistInput>
-            )}
-            <InputGroup>
-                {localTask.checkList.map((content, id) => (
-                    <ChecklistContent key={id}>
-                        <>{id + 1}</>
-                        {isEdit ? (
-                            <>
-                                <ModalInput
-                                    placeholder={content.text}
-                                    onChange={(e) =>
-                                        editItem(
-                                            e.target.value,
-                                            id,
-                                            localTask,
-                                            change,
-                                            setChange
-                                        )
-                                    }
-                                    value={localTask.checkList[id].text}
-                                />
-                                <IconsModal
-                                    onClick={() => {
-                                        localTask.checkList.splice(id, 1);
-                                        setChange(!change);
-                                    }}
-                                >
-                                    <DeleteFilled />
-                                </IconsModal>{" "}
-                            </>
-                        ) : (
-                            <>
-                                <Checkbox
-                                    disabled={isFinish}
-                                    type="checkbox"
-                                    id={`content${id}`}
-                                    checked={content.checked}
-                                    onChange={(e) =>
-                                        handleChangeCheck(
-                                            content,
-                                            e,
-                                            updateDBTasks,
-                                            change,
-                                            setChange
-                                        )
-                                    }
-                                />
-                                <LabelModal
-                                    htmlFor={`content${id}`}
-                                    checked={content.checked}
-                                >
-                                    {content.text}
-                                </LabelModal>
-                            </>
-                        )}
-                    </ChecklistContent>
-                ))}
-            </InputGroup>
-        </>
-    );
-
-    const dateDescription = (isFinish) =>
-        isEdit ? (
-            <DatePicker
-                showTime={{ format: "HH:mm" }}
-                format="YYYY-MM-DD HH:mm"
-                onChange={(ev, e) =>
-                    setLocalTask((prevState) => ({
-                        ...prevState,
-                        date: e,
-                    }))
-                }
-            />
-        ) : (
-            <ShowDate>
-                {localTask.date.length > 0 && (
-                    <>
-                        <div>Data para conclusão:</div>
-                        <div>
-                            {moment(localTask?.date).format(
-                                "DD  MMMM YYYY, h:mm a"
-                            )}
-                        </div>
-                    </>
-                )}
-                {isFinish && (
-                    <>
-                        <div>Concluído dia:</div>
-                        <div>
-                            {moment(
-                                allTaskDone[id].endDate
-                                    ? moment(allTaskDone[id].endDate)
-                                    : moment(allTaskDone[id].date)
-                            ).format("DD  MMMM YYYY, h:mm a")}
-                        </div>
-                    </>
-                )}
-            </ShowDate>
-        );
-
     if (localTask) {
-        if (status === "todo") {
-            return (
-                <Modal
-                    visible={isModalVisible}
-                    onCancel={handleCancel}
-                    footer={null}
-                    closable={false}
-                    style={{ cursor: "default" }}
-                >
-                    <InfoModal>
-                        <HeaderModal>
-                            <ModalInput
-                                disabled={!isEdit}
-                                placeholder={allTask[id].title}
-                                onChange={(e) =>
-                                    setLocalTask((prevState) => ({
-                                        ...prevState,
-                                        title: e.target.value,
-                                    }))
-                                }
-                                value={localTask.title}
-                                type="2em"
-                                isEdit="1px"
-                                border={isEdit ? "#8080805f" : null}
-                            />
-                            {isEdit ? (
-                                <PriorityDropdown
-                                    localTask={localTask}
-                                    setLocalTask={setLocalTask}
-                                />
-                            ) : (
-                                <ContainerPriority
-                                    color={priorityColor(localTask)}
-                                >
-                                    <DotPriority
-                                        color={priorityColor(localTask)}
-                                    />
-                                    {priorityText(localTask)}
-                                </ContainerPriority>
-                            )}
-                        </HeaderModal>
-
-                        {detailDescription()}
-                        {listDescription()}
-                        {dateDescription()}
-                        {isEdit ? (
-                            <ButtonModal
-                                onClick={() =>
-                                    handleEditConfirm(
-                                        id,
-                                        localTask,
-                                        allTask,
-                                        setIsEdit,
-                                        updateDBTasks,
-                                        change,
-                                        setChange
-                                    )
-                                }
-                                type="confirm"
-                            >
-                                CONFIRMAR
-                            </ButtonModal>
-                        ) : (
-                            <ButtonModal
-                                type="alert"
-                                onClick={() => setIsEdit(true)}
-                            >
-                                EDITAR
-                            </ButtonModal>
-                        )}
-                        <ButtonModal
-                            type="cancel"
-                            onClick={(e) => handleCancel()}
-                        >
-                            FECHAR
-                        </ButtonModal>
-                    </InfoModal>
-                </Modal>
-            );
-        }
-        if (status === "done") {
-            return (
-                <Modal
-                    visible={isModalVisible}
-                    onCancel={handleCancel}
-                    footer={null}
-                    width={300}
-                    closable={false}
-                    style={{ cursor: "default" }}
-                >
-                    <InfoModal>
-                        <HeaderModal>
-                            <ModalInput
-                                disabled
-                                value={localTask.title}
-                                type="2em"
-                            />
-                            <ContainerPriority color={priorityColor(localTask)}>
-                                <DotPriority color={priorityColor(localTask)} />
-                                {priorityText(localTask)}
-                            </ContainerPriority>
-                        </HeaderModal>
-                        {detailDescription()}
-                        {listDescription(true)}
-                        {dateDescription(true)}
-                        <ButtonModal
-                            type="cancel"
-                            onClick={(e) => handleCancel()}
-                        >
-                            FECHAR
-                        </ButtonModal>
-                    </InfoModal>
-                </Modal>
-            );
-        }
+        return (
+            <Modal
+                visible={isModalVisible}
+                onCancel={() => setModalVisible(false)}
+                footer={null}
+                closable={false}
+                style={{ cursor: "default" }}
+            >
+                <TodoForm
+                    isEdit={isEdit}
+                    setIsEdit={setIsEdit}
+                    id={id}
+                    setId={setId}
+                    status={status}
+                    localTask={localTask}
+                    setLocalTask={setLocalTask}
+                    isModal={true}
+                    setModalVisible={setModalVisible}
+                />
+            </Modal>
+        );
     }
 }
